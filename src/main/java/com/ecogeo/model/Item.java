46e4c85d8001ccfaa858a6643501f7fb58cb1705
shane <tjdghks0531@gmail.com>
2018-03-11 23:58:01 +0900

    모델 item추가

---
+++ src/main/java/com/ecogeo/model/Item.java
@@ -0,0 +1,81 @@
+package com.ecogeo.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Item {
+  @Id
+  @GeneratedValue
+  int idx;
+
+  //계
+  String species;
+
+  //세부분류군명
+  String detailSpecies;
+
+  //학명
+  String scientificName;
+
+  String scientificKorName;
+
+  String lifeType;
+
+  //문
+  String phylumName;
+
+  //목
+  String orderName;
+
+  //과
+  String familyName;
+
+  //명명자
+  String speciesSimpleName;
+
+  String realName;
+
+  String subSpeciese;
+
+  String variety;
+
+  String formaCheck;
+
+  String formaName;
+
+  //멸종위기
+  Boolean propCrisis;
+
+  //희귀
+  String propRare;
+
+  //특산
+  Boolean propSpecialty;
+
+  //귀화
+  Boolean propNatural;
+
+  //생태계교란
+  Boolean propDerange;
+
+  //도래유형
+  String propAdvent;
+
+  //천연기념물
+  Boolean propMonument;
+
+  //고유종
+  Boolean propOrigin;
+
+  //외래종
+  Boolean propAlien;
+
+  //국외반출승인종
+  Boolean propAoea;
+
+}
